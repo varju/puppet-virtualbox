430caecf93e9709fd1530a8a56cd539609100ea8
Alex Varju <alex@varju.ca>
2013-03-23 13:26:21 -0700

    Include the version number in the package name
    This ensures that existing packages will be updated to the latest version, as described in http://projects.puppetlabs.com/projects/1/wiki/Package_Management_With_Dmg_Patterns

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class virtualbox {
-  package { 'VirtualBox':
+  package { 'VirtualBox-4.2.10':
     provider => 'pkgdmg',
     source   => 'http://download.virtualbox.org/virtualbox/4.2.10/VirtualBox-4.2.10-84104-OSX.dmg'
   }
